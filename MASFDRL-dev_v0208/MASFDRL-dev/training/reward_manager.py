857b0a57629347572a97f2b7447df231067f126b
smoothwang <159822443+smoothwang@users.noreply.github.com>
2025-02-25 02:11:05 +0800

    Delete MASFDRL-dev_v0208/MASFDRL-dev directory

--- MASFDRL-dev_v0208/MASFDRL-dev/training/reward_manager.py
+++
@@ -1,8 +0,0 @@
-class RewardManager:
-    def discount(self, rewards, gamma):
-        discounted_rewards = []
-         R = 0
-         for r in reversed(rewards):
-             R = r + gamma * R
-             discounted_rewards.insert(0, R)
-         return torch.tensor(discounted_rewards)
